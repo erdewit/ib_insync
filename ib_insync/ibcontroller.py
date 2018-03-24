0c3ce09375209d9e3823aa21b22fecefd56b5358
erdewit <ewald.de.wit@gmail.com>
2018-03-24 17:55:25 +0100

    set explicit environment for IBC subprocess

--- ib_insync/ibcontroller.py
+++ ib_insync/ibcontroller.py
@@ -156,6 +156,7 @@ class IBC(Object):
 
         # run shell command
         self._proc = await asyncio.create_subprocess_shell(cmd,
+                env=os.environ,
                 stdout=asyncio.subprocess.PIPE)
         self._monitor = asyncio.ensure_future(self.monitorAsync())
 
