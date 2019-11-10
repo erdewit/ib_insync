ca9ca6d42e5358bd468304ba7cd300e86e07a58c
erdewit <ewald.de.wit@gmail.com>
2019-11-10 19:18:16 +0100

    flake8 fix

--- ib_insync/util.py
+++ ib_insync/util.py
@@ -53,12 +53,12 @@ def df(objs, labels=None):
 def isnamedtupleinstance(x):
     t = type(x)
     b = t.__bases__
-    if len(b) != 1 or b[0] != tuple: 
+    if len(b) != 1 or b[0] != tuple:
         return False
     f = getattr(t, '_fields', None)
-    if not isinstance(f, tuple): 
+    if not isinstance(f, tuple):
         return False
-    return all(type(n)==str for n in f)
+    return all(type(n) == str for n in f)
 
 
 def tree(obj):
