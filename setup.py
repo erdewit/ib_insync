9e16f04b4b98e093a4091aab7fbfe65d1da0a8eb
Steffen <28284489+stnatter@users.noreply.github.com>
2022-01-20 09:15:42 +0800

    adding Python 3.10

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
     ],
     keywords='ibapi tws asyncio jupyter interactive brokers async',
